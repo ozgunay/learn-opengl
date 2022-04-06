3544cae9128769b03c01bbd33537ad7beab12923
Özgün AY <ozgunay2@hotmail.com>
2022-04-06 09:44:46 +0300

    Added default configuration

--- src/main.cpp
+++ src/main.cpp
@@ -1,4 +1,5 @@
 #include <Magnum/GL/DefaultFramebuffer.h>
+#include <Magnum/GL/Version.h>
 #include <Magnum/Platform/GlfwApplication.h>
 
 using namespace Magnum;
@@ -11,7 +12,11 @@ class MyApplication: public Platform::Application {
         void drawEvent() override;
 };
 
-MyApplication::MyApplication(const Arguments& arguments): Platform::Application{arguments} {
+MyApplication::MyApplication(const Arguments& arguments):
+    Platform::Application{ arguments, Configuration{}
+        .setTitle("Learn-OpenGL with Magnum Engine")
+        .setWindowFlags(Configuration::WindowFlag::Resizable),
+    GLConfiguration{}.setVersion(GL::Version::GL330) } {
     /* TODO: Add your initialization code here */
 }
 
